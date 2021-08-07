8c931ba5675543a282284c3ba8f7d20851ca7850
Archita Racherla <archita.racherla@gmail.com>
2021-08-07 17:07:22 -0500

    navBar, footer, and NavBar components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+// import About from "../components/about";
+// import Portfolio from "../components/portfolio";
+// import Contact from "../components/contact";
+// import Resume from "../components/resume";
+import NavBar from "../components/Nav";
+
+function Header() {
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/about" />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/resume">
+          <Resume />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Header;
