dbf5e304a0a8f21ef3a10a22b95919d895b209f5
Archita Racherla <archita.racherla@gmail.com>
2021-08-08 23:28:57 -0500

    readme finished

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # React-Portfolio
+
+## Purpose
+
+This application is a portfolio of my projects. It also includes an about me, contact, resume, and project sections.
+
+## Built With
+
+- JavaScript
+- React
+- Express.js
+- IndexedDb
+- Manifest.json
+- service-worker.js
+
+## Website
+
+![](src/assets/images/screenshot.JPG)
+
+## Contribution
+
+Made by Archita
---
+++ build/precache-manifest.f01c4bbe64e1d183a033fd736df708b6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba4178f9d7e7080e09ffa18a1508431",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "e035d2fb7223f7163c15",
+    "url": "/React-Portfolio/static/css/2.86fa2864.chunk.css"
+  },
+  {
+    "revision": "d6e584cbfd9313cb0d9a",
+    "url": "/React-Portfolio/static/css/main.cb3c6c01.chunk.css"
+  },
+  {
+    "revision": "e035d2fb7223f7163c15",
+    "url": "/React-Portfolio/static/js/2.db61a0e9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-Portfolio/static/js/2.db61a0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e584cbfd9313cb0d9a",
+    "url": "/React-Portfolio/static/js/main.512a1e9c.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "60878177643598023a64d0809bf26de2",
+    "url": "/React-Portfolio/static/media/EventBus.60878177.JPG"
+  },
+  {
+    "revision": "4ed80dff7b9f19ca84181f2d6c7a43ea",
+    "url": "/React-Portfolio/static/media/Resume.4ed80dff.docx"
+  },
+  {
+    "revision": "894e7fe95c1f9c1bc4f89cbb6b1d83f5",
+    "url": "/React-Portfolio/static/media/movieReddy.894e7fe9.JPG"
+  },
+  {
+    "revision": "a187e0cede29dc276f46251d3029985e",
+    "url": "/React-Portfolio/static/media/notes.a187e0ce.JPG"
+  },
+  {
+    "revision": "558fb82acbda6a5d16abd1ceb7766025",
+    "url": "/React-Portfolio/static/media/runBuddy.558fb82a.JPG"
+  },
+  {
+    "revision": "38f884d751c59674f1c907f222b2c37a",
+    "url": "/React-Portfolio/static/media/weather.38f884d7.JPG"
+  },
+  {
+    "revision": "787bb095220890bc08281b2322558882",
+    "url": "/React-Portfolio/static/media/workday.787bb095.JPG"
+  }
+]);
